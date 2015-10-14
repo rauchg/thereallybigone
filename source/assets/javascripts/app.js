cd1844039101031f2f42d20052d8ab43763ec0d9
Marcel <marcel.kalveram@googlemail.com>
2015-10-14 17:09:56 +0200

    added geolocation detection, removed viewBox animations in SVG, replaced them with CSS animation via JS

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -2,29 +2,90 @@
 $(document).foundation();
 // -----------------------------------------------------------------------------
 
+var geolocationAvailable = false;
 
 // Button trigger for the quake duration progress bar
 $(".quake-timer").click(function () {
   $(".custom-meter").animate({width:"100%"});
 });
 
+// check if geolocation is available
+if (Modernizr.geolocation) {
+  geolocationAvailable = true;
+}
+
+// check if user is within affected area
+$('.js-button-risk').on('click', function() {
+
+  if ( ! geolocationAvailable) {
+    // message that user's browser doesn't support geolocation
+    return;
+  }
+
+  $('.js-button-risk .js-loading').show();
+
+  // get coordinates, this will prompt user to give access
+  navigator.geolocation.getCurrentPosition(function(position) {
+
+    $('.js-button-risk .js-loading').hide();
+
+    // save user's latitude and longitude
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+
+    // north west bounds
+    var nwBoundLat = 48.392738;
+    var nwBoundLng = -124.727783;
+
+    // south east bounds
+    var seBoundLat = 46.141783;
+    var seBoundLng = -122.904053;
+
+    // if geolocation is within bounds, show warning
+    if (nwBoundLat > lat > seBoundLat &&
+        nwBoundLng < lng < seBoundLng) {
+      $('.js-alert-box-danger').show();
+    }
+
+    // show information when user is not in area
+    else {
+      $('.js-alert-box-safe').show();
+    }
+
+  });
+
+});
 
 // Hiding and re-triggering animation of the elements
 // Makes animation also work in reverse
 $(window).scroll(function () {
+
+  var $fixedHeader = $('.fixed-header');
+
   $('.animation-test').each(function () {
       var Pos = $(this).offset().top;
       var Height = 600;
       var topOfWindow = $(window).scrollTop();
+
+      // topOfWindow is the current scroll position and can be seen in the console
+      console.log(topOfWindow);
+
+      // animation only happens until the threshold, after that is stays where it is
+      if (topOfWindow < 2000) {
+        // this is how you would achieve parallax for any element (using negative position on fixed elements),
+        // replace 2 by any other number to define scroll speed
+        $fixedHeader.css('top', -topOfWindow / 2);
+      }
+
       // var bottomOfWindow = $(window).scrollTop() + $(window).height();
 
-      if (Pos < topOfWindow + Height && Pos + Height > topOfWindow) {
-        $(this).addClass(" fadeInDown animation-test--visible");
-      } else {
-        $(this).removeClass("fadeInDown animation-test--visible");
-        // TODO: testing the 'out' animation
-        // $(this).stop(true, true).delay(1200).animate({opacity:"0"});
-      }
+      // if (Pos < topOfWindow + Height && Pos + Height > topOfWindow) {
+      //   $(this).addClass(" fadeInDown animation-test--visible");
+      // } else {
+      //   $(this).removeClass("fadeInDown animation-test--visible");
+      //   // TODO: testing the 'out' animation
+      //   // $(this).stop(true, true).delay(1200).animate({opacity:"0"});
+      // }
 
       // TODOs:
       // 1. We need to be able to pass an animation class into this from HTML
