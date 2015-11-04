d87026d06f1a66de3cbf27bf2d8672a17b7d77bb
Sergei Tararinov <sergei@hanno.co>
2015-11-04 18:16:57 +0800

    Try implementing CSS pie charts

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -600,4 +600,16 @@ $(document).ready(function() {
 
   });
 
+  // PIE CHART
+  function $$(selector, context) {
+    context = context || document;
+    var elements = context.querySelectorAll(selector);
+    return Array.prototype.slice.call(elements);
+  }
+
+  $$('.pie').forEach(function(pie) {
+    var p = pie.textContent;
+    pie.style.animationDelay = '-' + parseFloat(p) + 's';
+  });
+
 });
