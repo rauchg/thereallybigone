5d9660447e9c92957263486337a4dc2f5ecf6704
Dan Zajdband <dan.zajdband@gmail.com>
2016-03-23 15:49:30 -0400

    Using gulp-sass for assets.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-scss');
+var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
